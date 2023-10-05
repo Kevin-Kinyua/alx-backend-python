4b2e08097ab7a7ae2becf57d7ae54855d9fba24e
Kevin-Kinyua <w.kevinkinyua@gmail.com>
2023-10-05 04:39:46 -0700

    Task corrections

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -5,14 +5,9 @@ pi, a float with a value of 3.14
 i_understand_annotations, a boolean with a value of True
 school, a string with a value of “Holberton”
 """
-# An integer variable 'a' with a value of 1
-a = 1
 
-# A float variable 'pi' with a value of 3.14
-pi = 3.14
 
-# A boolean variable 'i_understand_annotations' with a value of True
-i_understand_annotations = True
-
-# A string variable 'school' with a value of "Holberton"
-school = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = 'Holberton'
--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,18 +1,13 @@
- #!/usr/bin/env python3
+#!/usr/bin/env python3
 """Write a type-annotated function sum_list which takes a list
 input_list of floats as argument and returns their sum as a float.
 """
 
+
 from typing import List
 
+
 def sum_list(input_list: List[float]) -> float:
-    """
-    Calculate the sum of a list of floats.
-
-    Args:
-        input_list (List[float]): A list of floats to be summed.
-
-    Returns:
-        float: The sum of the elements in the input list.
-    """
-    return sum(input_list)
+    '''returns their sum as a float
+    '''
+    return float(sum(input_list))
