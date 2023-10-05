08bd0188f7aad2cf33382a71476f4c951f91fd39
Kevin-Kinyua <w.kevinkinyua@gmail.com>
2023-10-05 04:33:05 -0700

    Task 4 & 5 corrections

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,7 +3,16 @@
 input_list of floats as argument and returns their sum as a float.
 """
 
-
 from typing import List
 
+def sum_list(input_list: List[float]) -> float:
+    """
+    Calculate the sum of a list of floats.
 
+    Args:
+        input_list (List[float]): A list of floats to be summed.
+
+    Returns:
+        float: The sum of the elements in the input list.
+    """
+    return sum(input_list)
